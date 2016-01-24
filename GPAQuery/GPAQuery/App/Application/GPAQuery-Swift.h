88aea9c472cc98cd3aee3a28352e9a4ec049b739
BigPi <wangdapishuai@163.com>
2016-01-24 22:49:37 +0800

    remove lib PNChart add swift lib ios-Charts unexpectedly oc can add swift library

---
+++ GPAQuery/GPAQuery/App/Application/GPAQuery-Swift.h
@@ -0,0 +1,13 @@
+//
+//  GPAQuery-Swift.h
+//  GPAQuery
+//
+//  Created by pi on 16/1/24.
+//  Copyright © 2016年 pi. All rights reserved.
+//
+
+#ifndef GPAQuery_Swift_h
+#define GPAQuery_Swift_h
+
+
+#endif /* GPAQuery_Swift_h */
