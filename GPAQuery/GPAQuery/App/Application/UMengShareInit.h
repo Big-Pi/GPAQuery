08f132ccd6272a1d4e4636ffa32efc9b4a642189
BigPi <wangdapishuai@163.com>
2016-01-27 13:29:36 +0800

    setup basic share function

---
+++ GPAQuery/GPAQuery/App/Application/UMengShareInit.h
@@ -0,0 +1,13 @@
+//
+//  UMengShareInit.h
+//  GPAQuery
+//
+//  Created by pi on 16/1/27.
+//  Copyright © 2016年 pi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UMengShareInit : NSObject
+
+@end
---
+++ GPAQuery/GPAQuery/App/Application/UMengShareInit.m
@@ -0,0 +1,13 @@
+//
+//  UMengShareInit.m
+//  GPAQuery
+//
+//  Created by pi on 16/1/27.
+//  Copyright © 2016年 pi. All rights reserved.
+//
+
+#import "UMengShareInit.h"
+
+@implementation UMengShareInit
+
+@end
